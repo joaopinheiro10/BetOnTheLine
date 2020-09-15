5f7860e42375f2cc51b3b1d6cac280a22d8dfa38
Luis-Trigueiro <57730922+Luis-Trigueiro@users.noreply.github.com>
2020-09-15 18:05:32 +0100

    work-on

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -1,29 +1,16 @@
 import React, { useState } from "react";
 import "./homepage.css";
 
-
-const Homepage = () =>{
-
-    
-        return (
-          <div className="Homepage">
-            
-        
-        
-        <form>
+const Homepage = () => {
+  return (
+    <div className="Homepage">
+      <form>
         <h3>Login</h3>
-            <input type= "text" name="email" placeholder="e-mail..."/>
-            <input type="password" name= "password" placeholder="Sua senha..."/>
-            <input type="submit" name="acao" value="Enviar"/>
-        
-        </form>
-    
-        
-        
-    
-        
-    
-          </div>
-        );
-}
+        <input type="text" name="email" placeholder="e-mail..." />
+        <input type="password" name="password" placeholder="Sua senha..." />
+        <input type="submit" name="acao" value="Enviar" />
+      </form>
+    </div>
+  );
+};
 export default Homepage;
