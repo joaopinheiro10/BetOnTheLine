30d6ac0e9b8a438437d6ead97da7d3bd823fe9f2
Luis-Trigueiro <57730922+Luis-Trigueiro@users.noreply.github.com>
2020-09-15 18:03:23 +0100

    starting work on

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homepage from './homepage/homepage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Building website</h1>
-      <button>Click me</button>
+      <Homepage/>
     </div>
   );
 }
---
+++ src/homepage/homepage.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./homepage.css";
+
+
+const Homepage = () =>{
+
+    
+        return (
+          <div className="Homepage">
+            
+        
+        
+        <form>
+        <h3>Login</h3>
+            <input type= "text" name="email" placeholder="e-mail..."/>
+            <input type="password" name= "password" placeholder="Sua senha..."/>
+            <input type="submit" name="acao" value="Enviar"/>
+        
+        </form>
+    
+        
+        
+    
+        
+    
+          </div>
+        );
+}
+export default Homepage;
