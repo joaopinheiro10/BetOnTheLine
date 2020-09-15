b2845e36e7bdaa5161248aebfea2c05dc876b2ce
Joao Pinheiro <joao_pinheiro10@hotmail.com>
2020-09-15 15:32:19 +0100

    added axios dependency so we can perform HTTP requests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Building website</h1>
+      <button>Click me</button>
     </div>
   );
 }
